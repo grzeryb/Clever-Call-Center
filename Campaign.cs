f6b62f4fbea7c31142030dc95417b787abe4e319
Dawid Wekwejt <dawid.wekwejt@hotmail.com>
2012-05-01 21:27:27 +0200

    Struktura Kampania

---
+++ Campaign.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clever_Call_Center
+{
+    public struct Campaign
+    {
+        string status;
+        string name;
+        string description;
+        DateTime campaignEndTime;
+        DateTime campaignStartTime;
+        DateTime timeOfCreation;
+        DateTime timeOfModification;
+    }
+}
---
+++ DBConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clever_Call_Center
+{
+    class DBConnection
+    {
+    }
+}
